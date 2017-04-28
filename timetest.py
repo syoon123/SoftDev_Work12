e3cf10ea086e02f2a0efde9a2e2e95a8b5217406
Maddie Ostergaard <mostergaard@stuy.edu>
2017-04-28 14:48:52 -0400

    got first wrapper prob to work

---
+++ timetest.py
@@ -0,0 +1,33 @@
+import time
+
+def wrapper( f ):
+    def inner( *arg ):       
+        print f.func_name + ": (" + str(arg[0]) + ",)" 
+        return f( *arg )
+    return inner
+
+def time(f):
+    def inner( *arg):
+        start = time.time()
+        f( *arg)
+        end = time.time()
+        return end - start
+    return inner
+    
+@wrapper
+def fact(num):
+    if num == 0:
+        return 1
+    else:
+        return fact(num - 1) * num
+
+print fact(5)
+
+@time
+def fib(num):
+    if num == 1 or num==2:
+        return 1
+    else:
+        return fib(num-1) + fib(num-2)
+
+print fib(6)
