fa9247760c90d030d2f70176eb4de56b61933950
Jordan Yaqoob <Jordan Yaqoob>
2017-04-29 19:28:55 -0400

    fixed time decorator

--- timetest.py
+++ timetest.py
@@ -6,7 +6,7 @@ def wrapper( f ):
         return f( *arg )
     return inner
 
-def time(f):
+def timelog(f):
     def inner( *arg):
         start = time.time()
         f( *arg)
@@ -23,11 +23,11 @@ def fact(num):
 
 print fact(5)
 
-@time
+@timelog
 def fib(num):
     if num == 1 or num==2:
         return 1
     else:
         return fib(num-1) + fib(num-2)
 
-print fib(6)
+print fib(32)
